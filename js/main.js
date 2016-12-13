0c0cdf867366e2ffab593153e9fbabad041be6c4
Rayhatron <rayhatron@gmail.com>
2016-12-13 20:57:20 +0200

    Added vuejs to allow dynamic creation of content
    Objects with information will be used to create multiple html elements with the same structure but different data.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+window.onload = function(){
+    var app = new Vue({
+    el: '#content',
+    data: {
+        message: 'Hello Vue!',
+        projects: [
+            {
+                name: "Project 1",
+                github: "https://github.com",
+                demo: "google.com", 
+                description: "Lorem ipsum",
+                tags: ["tag 1", "tag 2", "tag 3"]
+            }, 
+            {
+                name: "Project 2",
+                github: "github.com",
+                demo: "google.com", 
+                description: "Lorem ipsum ipsum",
+                tags: ["tag 1", "tag 2", "tag 3"]
+            },  
+            {
+                name: "Project 3",
+                github: "github.com",
+                demo: "google.com", 
+                description: "Lorem ipsum ipsum ipsum",
+                tags: ["tag 1", "tag 2", "tag 3"]
+            }, ]
+    }
+    })
+}
+
+/*
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
 function showMenu() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -13,3 +46,5 @@ function hideMenu(){
 		showMenu();
 	}, 100);
 }
+*/
+
