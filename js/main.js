a9546e47daa60c4f4c269a0c9d8baa2efe31ff38
Rayhatron <rayhatron@gmail.com>
2017-05-03 13:16:36 +0200

    Revamped design and structure

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 var menu = document.getElementById("myTopnav");
+var nav = document.getElementById("nav");
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleMenu() {
     if (menu.className === "topnav") {
-        menu.className += " responsive";
+        menu.classList.add("responsive");
     } else {
-        menu.className = "topnav";
+        menu.classList.remove("responsive");
     }
 }
 
 function hideMenu(){
 
     setTimeout( function(){
-        menu.className = "topnav";
+        menu.classList.remove("responsive");
     }, 500);
  
 }
+
+// Make the nav sticky when header is out of view. 
+window.onscroll = function() {
+    console.log("scrolling");
+    if( window.pageYOffset < 300){
+        console.log("not-fixed");
+        nav.classList.remove("navbar-fixed");
+    }
+    if( window.pageYOffset > 300){
+        console.log("fixed");
+        nav.classList.add("navbar-fixed");
+    }
+};
